783a518474d8b6b21acd3095d12501572905f62e
Sinval Luz <contato.sinvalluz@gmail.com>
2025-12-05 10:33:33 -0300

    feat: add Footer component with copyright notice

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+	return (
+		<footer className='text-center mt-8'>
+			<p className='text-white'>© Sinval Luz Souza Filho - Todos os direitos reservados. 2024 - ****.</p>
+		</footer>
+	);
+}
