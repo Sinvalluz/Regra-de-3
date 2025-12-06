771964555988343a7921a0fbdec638f3fb6698f5
Sinval Luz <contato.sinvalluz@gmail.com>
2025-12-05 21:46:53 -0300

    fix: adjust footer margin for improved layout

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
 	return (
-		<footer className='text-center mt-8'>
+		<footer className='text-center mt-5'>
 			<p className='text-white'>© Sinval Luz Souza Filho - Todos os direitos reservados. 2024 - ****.</p>
 		</footer>
 	);
