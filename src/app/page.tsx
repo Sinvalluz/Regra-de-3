9f8e60c6794f30a9274457a6b175a87a1e777108
Sinval Luz <contato.sinvalluz@gmail.com>
2025-12-13 22:13:28 -0300

    style: add a divider to the UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,11 @@ import Main from '../components/Main';
 
 export default function Home() {
 	return (
-		<div className='bg-black min-h-screen w-full flex justify-center items-center'>
-			<div>
-				<Header />
-				<Main />
-				<Footer />
-			</div>
+		<div className='bg-black min-h-screen w-full flex justify-center flex-col items-center'>
+			<Header />
+			<div className='h-px w-3/4 bg-white/10 mb-5 text-center'></div>
+			<Main />
+			<Footer />
 		</div>
 	);
 }
