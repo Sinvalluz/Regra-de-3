c8b07593be366edf2338baf07563b513fb8799dc
Sinval Luz <contato.sinvalluz@gmail.com>
2025-12-13 21:24:26 -0300

    style: change the text color in the footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,9 @@
 export default function Footer() {
 	return (
-		<footer className='text-center mt-5'>
-			<p className='text-white'>© Sinval Luz Souza Filho - Todos os direitos reservados. 2024 - ****.</p>
+		<footer className='mt-5'>
+			<p className='text-white/80 sm:text-base text-sm text-center'>
+				© Sinval Luz Souza Filho - Todos os direitos reservados. 2024 - ****.
+			</p>
 		</footer>
 	);
 }
