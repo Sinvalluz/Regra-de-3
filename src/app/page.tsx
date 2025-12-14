fabc0776e704f73fe2d3a13dc4797c8a2fe873bc
Sinval Luz <contato.sinvalluz@gmail.com>
2025-12-13 22:22:16 -0300

    style: dealing with divisor responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
 	return (
 		<div className='bg-black min-h-screen w-full flex justify-center flex-col items-center'>
 			<Header />
-			<div className='h-px w-3/4 bg-white/10 mb-5 text-center'></div>
+			<div className='h-px w-4/5 md:w-2/3 lg:w-1/2 2xl:w-2/5 bg-white/10 mb-5 text-center'></div>
 			<Main />
 			<Footer />
 		</div>
